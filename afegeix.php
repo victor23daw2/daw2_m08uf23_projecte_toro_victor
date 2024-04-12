cc03aa9dd3464a17eb5c555bd78b37305fb2930a
victor23daw2 <458371.clot@fje.edu>
2024-04-12 19:16:17 +0200

    afegir corregit

--- afegeix.php
+++ afegeix.php
@@ -13,59 +13,62 @@ if (!isset($_SESSION['adm'])) {
 ini_set('display_errors', 1);
 #Dades de la nova entrada
 #
-$uid = $_POST['uid'];
-$unorg = $_POST['unorg'];
-$num_id = $_POST['num_id'];
-$grup = $_POST['grup'];
-$dir_pers = $_POST['dir_pers'];
-$sh = $_POST['sh'];
-$cn = $_POST['cn'];
-$sn = $_POST['sn'];
-$nom = $_POST['nom'];
-$mobil = $_POST['mobil'];
-$adressa = $_POST['adressa'];
-$telefon = $_POST['telefon'];
-$titol = $_POST['titol'];
-$descripcio = $_POST['descripcio'];
-
-$objcl = array('inetOrgPerson', 'organizationalPerson', 'person', 'posixAccount', 'shadowAccount', 'top');
-#
-#Afegint la nova entrada
-$domini = 'dc=fjeclot,dc=net';
-$opcions = [
-  'host' => 'zend-vitoar.fjeclot.net',
-  'username' => "cn=admin,$domini",
-  'password' => 'fjeclot',
-  'bindRequiresDn' => true,
-  'accountDomainName' => 'fjeclot.net',
-  'baseDn' => 'dc=fjeclot,dc=net',
-];
-$ldap = new Ldap($opcions);
-$ldap->bind();
-$nova_entrada = [];
-Attribute::setAttribute($nova_entrada, 'objectClass', $objcl);
-Attribute::setAttribute($nova_entrada, 'uid', $uid);
-Attribute::setAttribute($nova_entrada, 'uidNumber', $num_id);
-Attribute::setAttribute($nova_entrada, 'gidNumber', $grup);
-Attribute::setAttribute($nova_entrada, 'homeDirectory', $dir_pers);
-Attribute::setAttribute($nova_entrada, 'loginShell', $sh);
-Attribute::setAttribute($nova_entrada, 'cn', $cn);
-Attribute::setAttribute($nova_entrada, 'sn', $sn);
-Attribute::setAttribute($nova_entrada, 'givenName', $nom);
-Attribute::setAttribute($nova_entrada, 'mobile', $mobil);
-Attribute::setAttribute($nova_entrada, 'postalAddress', $adressa);
-Attribute::setAttribute($nova_entrada, 'telephoneNumber', $telefon);
-Attribute::setAttribute($nova_entrada, 'title', $titol);
-Attribute::setAttribute($nova_entrada, 'description', $descripcio);
-$dn = 'uid=' . $uid . ',ou=' . $unorg . ',dc=fjeclot,dc=net';
-
-if ($ldap->add($dn, $nova_entrada)) {
-  echo "Usuari creat";
-  echo '<a href="https://zends-vitoar:443/projecte/menu.php">Torna al menú</a>';
-} else {
-  echo "Error al crear l'usuari";
-  echo '<a href="https://zends-vitoar:443/projecte/menu.php">Torna al menú</a>';
-}?>
+if(isset($_POST['uid'])){
+    $uid = $_POST['uid'];
+    $unorg = $_POST['unorg'];
+    $num_id = $_POST['num_id'];
+    $grup = $_POST['grup'];
+    $dir_pers = $_POST['dir_pers'];
+    $sh = $_POST['sh'];
+    $cn = $_POST['cn'];
+    $sn = $_POST['sn'];
+    $nom = $_POST['nom'];
+    $mobil = $_POST['mobil'];
+    $adressa = $_POST['adressa'];
+    $telefon = $_POST['telefon'];
+    $titol = $_POST['titol'];
+    $descripcio = $_POST['descripcio'];
+    
+    $objcl = array('inetOrgPerson', 'organizationalPerson', 'person', 'posixAccount', 'shadowAccount', 'top');
+    #
+    #Afegint la nova entrada
+    $domini = 'dc=fjeclot,dc=net';
+    $opcions = [
+        'host' => 'zend-vitoar.fjeclot.net',
+        'username' => "cn=admin,$domini",
+        'password' => 'fjeclot',
+        'bindRequiresDn' => true,
+        'accountDomainName' => 'fjeclot.net',
+        'baseDn' => 'dc=fjeclot,dc=net',
+    ];
+    $ldap = new Ldap($opcions);
+    $ldap->bind();
+    $nova_entrada = [];
+    Attribute::setAttribute($nova_entrada, 'objectClass', $objcl);
+    Attribute::setAttribute($nova_entrada, 'uid', $uid);
+    Attribute::setAttribute($nova_entrada, 'uidNumber', $num_id);
+    Attribute::setAttribute($nova_entrada, 'gidNumber', $grup);
+    Attribute::setAttribute($nova_entrada, 'homeDirectory', $dir_pers);
+    Attribute::setAttribute($nova_entrada, 'loginShell', $sh);
+    Attribute::setAttribute($nova_entrada, 'cn', $cn);
+    Attribute::setAttribute($nova_entrada, 'sn', $sn);
+    Attribute::setAttribute($nova_entrada, 'givenName', $nom);
+    Attribute::setAttribute($nova_entrada, 'mobile', $mobil);
+    Attribute::setAttribute($nova_entrada, 'postalAddress', $adressa);
+    Attribute::setAttribute($nova_entrada, 'telephoneNumber', $telefon);
+    Attribute::setAttribute($nova_entrada, 'title', $titol);
+    Attribute::setAttribute($nova_entrada, 'description', $descripcio);
+    $dn = 'uid=' . $uid . ',ou=' . $unorg . ',dc=fjeclot,dc=net';
+    
+    if ($ldap->add($dn, $nova_entrada)) {
+        echo "Usuari creat";
+        echo '<a href="https://zends-vitoar:443/projecte/menu.php">Torna al menú</a>';
+    } else {
+        echo "Error al crear l'usuari";
+        echo '<a href="https://zends-vitoar:443/projecte/menu.php">Torna al menú</a>';
+    }
+}
+?>
 <html>
   <head>
     <title>Crear</title>
